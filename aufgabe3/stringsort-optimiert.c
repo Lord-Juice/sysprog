2ccd1d86896104eb557d87dd28c04dce3ca5760e
Lord-Juice <kruesselpaul@gmail.com>
2024-11-21 09:40:18 +0100

    Finished Aufgabe 3

--- aufgabe3/stringsort-optimiert.c
+++ aufgabe3/stringsort-optimiert.c
@@ -3,28 +3,31 @@
 #include <string.h>
 #include <time.h>
 
-int compare(const void *a, const void *b) {
-    return strcmp(*(const char **)a, *(const char **)b);
-}
+#define m 12
 
 void bubblesort(void *base, size_t nitems, size_t size, int (*compar)(const void *, const void *)) {
-    char *a = base;
-    char *tmp = malloc(size);
+    char *a = (char *)base; // Cast zu char* für Berechnung von Offsets
+    char *tmp = malloc(size); // Temporärer Speicher für ein Element
     if (tmp == NULL) {
-        fprintf(stderr, "Speicherreservierung fehlgeschlagen\n");
+        fprintf(stderr, "Memory allocation failed\n");
         exit(1);
     }
+
     for (size_t i = nitems; i > 1; --i) {
         for (size_t j = 0; j < i - 1; ++j) {
-            if (compar(a + j * size, a + (j + 1) * size) > 0) {
-                memcpy(tmp, a + (j + 1) * size, size);
-                memcpy(a + (j + 1) * size, a + j * size, size);
-                memcpy(a + j * size, tmp, size);
+            // Vergleich über die übergebene Funktion compar
+            if (compar(&a[j * size], &a[(j + 1) * size]) > 0) {
+                // Elemente tauschen
+                memcpy(tmp, &a[(j + 1) * size], size);
+                memcpy(&a[(j + 1) * size], &a[j * size], size);
+                memcpy(&a[j * size], tmp, size);
             }
         }
     }
+
     free(tmp);
 }
+
 
 int main(int argc, const char *argv[]) {
     if (argc != 2) {
@@ -38,36 +41,36 @@ int main(int argc, const char *argv[]) {
         return 1;
     }
 
-    int m = 12; // maximale String-Länge inklusive Endezeichen
-    char *memory = malloc(n * m * sizeof(char));
-    if (memory == NULL) {
-        fprintf(stderr, "Speicherreservierung fehlgeschlagen\n");
+    char *a = malloc(n * m * sizeof(char));
+    if (a == NULL) {
+        fprintf(stderr, "Memory allocation failed\n");
         return 1;
     }
 
     srand(time(NULL));
+
+    printf("Unsortiertes Array:\n");
     for (int i = 0; i < n; ++i) {
-        int r = rand() % 1000;
-        snprintf(memory + i * m, m, "%d", r);
-        printf("%s ", memory + i * m);
+        int r = rand() % n;
+        snprintf(&a[i * m], m, "%d", r);
+        printf("%s ", &a[i * m]);
     }
     printf("\n");
 
-    // bubblesort direkt auf dem Speicherbereich aufrufen
-    bubblesort(memory, n, m, compare);
+    bubblesort(a, n, m, (int (*)(const void *, const void *))strcmp);
 
     printf("Sortiertes Array:\n");
-    printf("%s", memory);
+    printf("%s", &a[0]);
     for (int i = 1; i < n; ++i) {
-        if (strcmp(memory + i * m, memory + (i - 1) * m) == 0) {
+        if (strcmp(&a[i * m], &a[(i - 1) * m]) == 0) {
             printf("*");
         } else {
-            printf(" %s", memory + i * m);
+            printf(" %s", &a[i * m]);
         }
     }
     printf("\n");
 
-    free(memory);
+    free(a);
 
     return 0;
 }
